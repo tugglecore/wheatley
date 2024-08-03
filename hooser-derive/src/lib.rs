e81004030e22e246164a5de480fd871931c01d7a
tuggle <tuggledemetrius@gmail.com>
2024-08-02 21:21:53 -0400

    embed text retrieve from file into binary

--- hooser-derive/src/lib.rs
+++ hooser-derive/src/lib.rs
@@ -1,16 +1,21 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
+use std::fs;
+
 
 #[proc_macro_derive(Hooser)]
 pub fn hooser(tokens: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(tokens as DeriveInput);
     let identifer = ast.ident;
 
+    let bio = fs::read_to_string("./lucille.txt").unwrap();
+
     let gen = quote! {
         impl #identifer {
             fn bio (&self) -> &'static str {
-                "Poet"
+                // "Poet"
+                #bio
             }
         }
     };
