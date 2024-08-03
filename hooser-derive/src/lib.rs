d636b9c978ee222afe8ecb3436f34000889e182e
tuggle <tuggledemetrius@gmail.com>
2024-08-02 21:46:05 -0400

    embed toml data into binary

--- hooser-derive/src/lib.rs
+++ hooser-derive/src/lib.rs
@@ -2,6 +2,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 use std::fs;
+use toml::Table;
 
 
 #[proc_macro_derive(Hooser)]
@@ -9,7 +10,9 @@ pub fn hooser(tokens: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(tokens as DeriveInput);
     let identifer = ast.ident;
 
-    let bio = fs::read_to_string("./lucille.txt").unwrap();
+    let asset = fs::read_to_string("./lucille.toml").unwrap();
+    let asset = asset.parse::<Table>().unwrap();
+    let bio = asset["bio"].as_str();
 
     let gen = quote! {
         impl #identifer {
